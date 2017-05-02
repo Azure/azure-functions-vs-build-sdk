61a6d554e5afb6705a38ad839d5a3b4d67e9a83f
Vijay Ramakrishnan <vramak@microsoft.com>
2017-05-02 14:01:23 -0700

    Fixing the sample to have tooling package & fixing build.cmd

--- build.cmd
+++ build.cmd
@@ -3,5 +3,5 @@ dotnet build src\Microsoft.NET.Sdk.Functions --configuration=Release
 dotnet pack src\Microsoft.NET.Sdk.Functions --configuration=Release
 
 rmdir /S /Q %userprofile%\.nuget\packages\microsoft.net.sdk.functions
-dotnet restore sample\SampleFunctionApp\SampleFunctionApp.csproj
-msbuild sample\SampleFunctionApp\SampleFunctionApp.csproj /p:DeployOnBuild=true /p:configuration=Release
+dotnet restore sample\FunctionApp\FunctionApp\FunctionApp.csproj
+msbuild sample\FunctionApp\FunctionApp\FunctionApp.csproj /p:DeployOnBuild=true /p:configuration=Release
