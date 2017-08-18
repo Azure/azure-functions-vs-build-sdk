ab7445cb600de983f9ae45c23b746d5083eb5f56
Vijay Ramakrishnan <vramak@microsoft.com>
2017-08-18 09:56:48 -0700

    Updating the version to 1.0.2 and adding unit test projects to sample

---
+++ sample/NuGet/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace NuGet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
