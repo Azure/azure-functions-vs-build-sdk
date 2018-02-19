a1e970a323a05de68c100f90c4b0b51a4b6940d9
Maarten van Sambeek <maarten.van.sambeek@allego.eu>
2018-02-19 18:03:44 +0100

    Retrieving custom attributes through reflection-only load.
    This prevents System.IO.FileNotFoundExceptions when building a .NET Framework application using dotnet build.

--- src/Microsoft.NET.Sdk.Functions.Generator/AttributeExtensions.cs
+++ src/Microsoft.NET.Sdk.Functions.Generator/AttributeExtensions.cs
@@ -43,7 +43,7 @@ namespace MakeFunctionJson
             return attribute.GetType().GetCustomAttributes().Any(a => a.GetType().FullName == "Microsoft.Azure.WebJobs.Description.BindingAttribute")
                 || _supportedAttributes.Contains(attribute.GetType().Name);
 #else
-            return attribute.GetType().GetTypeInfo().GetCustomAttributes().Any(a => a.GetType().FullName == "Microsoft.Azure.WebJobs.Description.BindingAttribute")
+            return attribute.GetType().GetTypeInfo().GetCustomAttributesData().Any(a => a.AttributeType.FullName == "Microsoft.Azure.WebJobs.Description.BindingAttribute")
                 || _supportedAttributes.Contains(attribute.GetType().Name);
 #endif
         }
---
+++ src/Microsoft.NET.Sdk.Functions.Generator/CustomAttributeDataExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Microsoft.NET.Sdk.Functions.Generator
+{
+    public static class CustomAttributeDataExtensions
+    {
+        public static Attribute ConvertToAttribute(this CustomAttributeData data)
+        {
+            var attribute = data.Constructor.Invoke(data.ConstructorArguments.Select(arg => arg.Value).ToArray()) as Attribute;
+
+            foreach (var namedArgument in data.NamedArguments)
+            {
+                (namedArgument.MemberInfo as PropertyInfo)?.SetValue(attribute, namedArgument.TypedValue.Value, null);
+                (namedArgument.MemberInfo as FieldInfo)?.SetValue(attribute, namedArgument.TypedValue.Value);
+            }
+
+            return attribute;
+        }
+    }
+}
