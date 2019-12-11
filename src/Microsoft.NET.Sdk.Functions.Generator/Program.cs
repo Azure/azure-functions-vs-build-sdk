7202dfa30f19eb6bb45c5d7d6da20b34fc19604c
Brett Samblanet <brettsam@microsoft.com>
2019-12-11 10:44:38 -0800

    improving assembly location logic for FunctionGenerator

--- src/Microsoft.NET.Sdk.Functions.Generator/Program.cs
+++ src/Microsoft.NET.Sdk.Functions.Generator/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.Loader;
 using MakeFunctionJson;
 
@@ -25,7 +26,29 @@ namespace Microsoft.NET.Sdk.Functions.Console
 
                 AssemblyLoadContext.Default.Resolving += (context, assemblyName) =>
                 {
-                    return context.LoadFromAssemblyPath(Path.Combine(assemblyDir, assemblyName.Name + ".dll"));
+                    string assemblyPath = Path.Combine(assemblyDir, assemblyName.Name + ".dll");
+
+                    if (File.Exists(assemblyPath))
+                    {
+                        Assembly assembly = AssemblyLoadContext.Default.LoadFromAssemblyPath(assemblyPath);
+                        return assembly;
+                    }
+
+                    try
+                    {
+                        // If the assembly file is not found, it may be a runtime assembly for a different
+                        // runtime version (i.e. the Function app assembly targets .NET Core 3.1, yet this
+                        // process is running 3.0). In that case, just try to return the currently-loaded assembly,
+                        // even if it's the wrong version; we won't be running it, just reflecting.
+                        Assembly assembly = Assembly.Load(assemblyName.Name);
+                        return assembly;
+                    }
+                    catch (Exception)
+                    {
+                        // We'll already log an error if this happens; this gives a little more details if debug is enabled.
+                        logger.LogError($"Unable to find fallback for assembly '{assemblyName}'.");
+                        return null;
+                    }
                 };
 
                 IEnumerable<string> excludedFunctionNames = Enumerable.Empty<string>();
