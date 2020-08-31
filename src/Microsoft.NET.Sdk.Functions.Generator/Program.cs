6d820090d6635cc9a6ae5fa1dcfe91a8c2b9e5ec
Brett Samblanet <brettsam@microsoft.com>
2020-08-31 14:17:33 -0700

    fixing issue with args passed to Functions.Generator

--- src/Microsoft.NET.Sdk.Functions.Generator/Program.cs
+++ src/Microsoft.NET.Sdk.Functions.Generator/Program.cs
@@ -21,9 +21,7 @@ namespace Microsoft.NET.Sdk.Functions.Console
             }
             else
             {
-                var assemblyPath = args[0].Trim();
-                var outputPath = args[1].Trim();
-                var functionsInDependencies = bool.Parse(args[2].Trim());
+                ParseArgs(args, out string assemblyPath, out string outputPath, out bool functionsInDependencies, out IEnumerable<string> excludedFunctionNames);
                 var assemblyDir = Path.GetDirectoryName(assemblyPath);
 
 #if NETCOREAPP2_1
@@ -36,20 +34,28 @@ namespace Microsoft.NET.Sdk.Functions.Console
                 {
                     return Assembly.LoadFrom(Path.Combine(assemblyDir, e.Name + ".dll"));
                 };
-#endif
-                IEnumerable<string> excludedFunctionNames = Enumerable.Empty<string>();
-
-                if (args.Length > 2)
-                {
-                    var excludedFunctionNamesArg = args[2].Trim();
-                    excludedFunctionNames = excludedFunctionNamesArg.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
-                }
+#endif     
 
                 var converter = new FunctionJsonConverter(logger, assemblyPath, outputPath, functionsInDependencies, excludedFunctionNames);
                 if (!converter.TryRun())
                 {
                     logger.LogError("Error generating functions metadata");
                 }
+            }
+        }
+
+        internal static void ParseArgs(string[] args, out string assemblyPath, out string outputPath, out bool functionsInDependencies, out IEnumerable<string> excludedFunctionNames)
+        {
+            assemblyPath = args[0].Trim();
+            outputPath = args[1].Trim();
+            functionsInDependencies = bool.Parse(args[2].Trim());
+
+            excludedFunctionNames = Enumerable.Empty<string>();
+
+            if (args.Length > 3)
+            {
+                var excludedFunctionNamesArg = args[3].Trim();
+                excludedFunctionNames = excludedFunctionNamesArg.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
             }
         }
     }
