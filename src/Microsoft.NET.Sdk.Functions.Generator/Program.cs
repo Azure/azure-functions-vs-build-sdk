2288ae3e5738c526eaabf314ac4ca9513e25e8ba
Brett Samblanet <brettsam@microsoft.com>
2020-08-31 14:29:03 -0700

    fixing issue with args passed to Functions.Generator

--- src/Microsoft.NET.Sdk.Functions.Generator/Program.cs
+++ src/Microsoft.NET.Sdk.Functions.Generator/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.Loader;
 using MakeFunctionJson;
 
 namespace Microsoft.NET.Sdk.Functions.Console
@@ -18,24 +16,28 @@ namespace Microsoft.NET.Sdk.Functions.Console
             }
             else
             {
-                var assemblyPath = args[0].Trim();
-                var assemblyDir = Path.GetDirectoryName(assemblyPath);
-                var outputPath = args[1].Trim();
-                var functionsInDependencies = bool.Parse(args[2].Trim());
-
-                IEnumerable<string> excludedFunctionNames = Enumerable.Empty<string>();
-
-                if (args.Length > 2)
-                {
-                    var excludedFunctionNamesArg = args[2].Trim();
-                    excludedFunctionNames = excludedFunctionNamesArg.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
-                }
+                ParseArgs(args, out string assemblyPath, out string outputPath, out bool functionsInDependencies, out IEnumerable<string> excludedFunctionNames);
 
                 var converter = new FunctionJsonConverter(logger, assemblyPath, outputPath, functionsInDependencies, excludedFunctionNames);
                 if (!converter.TryRun())
                 {
                     logger.LogError("Error generating functions metadata");
                 }
+            }
+        }
+
+        internal static void ParseArgs(string[] args, out string assemblyPath, out string outputPath, out bool functionsInDependencies, out IEnumerable<string> excludedFunctionNames)
+        {
+            assemblyPath = args[0].Trim();
+            outputPath = args[1].Trim();
+            functionsInDependencies = bool.Parse(args[2].Trim());
+
+            excludedFunctionNames = Enumerable.Empty<string>();
+
+            if (args.Length > 3)
+            {
+                var excludedFunctionNamesArg = args[3].Trim();
+                excludedFunctionNames = excludedFunctionNamesArg.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
             }
         }
     }
---
+++ test/Microsoft.NET.Sdk.Functions.Generator.Tests/ArgumentsTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Microsoft.NET.Sdk.Functions.Console;
+using Xunit;
+
+namespace Microsoft.NET.Sdk.Functions.Test
+{
+    public class ArgumentsTests
+    {
+        [Fact]
+        public void NoExcludedFunctions()
+        {
+            var args = new[] { @"assemblyPath", "outputPath", "true" };
+
+            Program.ParseArgs(args, out string assemblyPath, out string outputPath, out bool functionsInDependencies, out IEnumerable<string> excludedFunctionNames);
+            Assert.Equal("assemblyPath", assemblyPath);
+            Assert.Equal("outputPath", outputPath);
+            Assert.True(functionsInDependencies);
+            Assert.Empty(excludedFunctionNames);
+        }
+
+        [Fact]
+        public void ExcludedFunctions()
+        {
+            var args = new[] { @"assemblyPath", "outputPath", "false", "Function1;Function2" };
+
+            Program.ParseArgs(args, out string assemblyPath, out string outputPath, out bool functionsInDependencies, out IEnumerable<string> excludedFunctionNames);
+            Assert.Equal("assemblyPath", assemblyPath);
+            Assert.Equal("outputPath", outputPath);
+            Assert.False(functionsInDependencies);
+            Assert.Collection(excludedFunctionNames,
+                s => Assert.Equal("Function1", s),
+                s => Assert.Equal("Function2", s));
+        }
+    }
+}
